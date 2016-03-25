d468c62bb1df80bd77ff52cf400049fec5c3c899
melezhik <melezhik@gmail.com>
2016-03-25 15:17:00 +0300

    up

--- story.pm
+++ story.pm
@@ -2,12 +2,12 @@ use Time::Piece;
 use DateTime;
 
 my $out = <<HERE;
-sequence_number=12345,remote_client=sapserver,2016-03-25 13:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
-sequence_number=12345,remote_client=sapserver,2016-03-25 13:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
-sequence_number=12345,remote_client=sapserver,2016-03-25 13:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
-sequence_number=12345,remote_client=sapserver,2016-03-25 13:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
-sequence_number=12345,remote_client=sapserver,2016-03-25 13:36:44:782 EDT,messageID=1002,user=jdoe4@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
-sequence_number=12345,remote_client=sapserver,2016-03-25 13:36:44:782 EDT,messageID=1002,user=jdoe3@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=5,result_action=Login Failure,result_reason=Invalid Password
+sequence_number=12345,remote_client=sapserver,2016-03-25 11:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
+sequence_number=12345,remote_client=sapserver,2016-03-25 11:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
+sequence_number=12345,remote_client=sapserver,2016-03-25 11:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
+sequence_number=12345,remote_client=sapserver,2016-03-25 11:36:44:782 EDT,messageID=1002,user=jdoe@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
+sequence_number=12345,remote_client=sapserver,2016-03-25 12:36:44:782 EDT,messageID=1002,user=jdoe4@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=2,result_action=Login Failure,result_reason=Invalid Password
+sequence_number=12345,remote_client=sapserver,2016-03-25 12:36:44:782 EDT,messageID=1002,user=jdoe3@example.com,client_ip_address=10.129.220.45,client_port=10250,browser_ip_address=x.x.x.x,result_code=5,result_action=Login Failure,result_reason=Invalid Password
 HERE
 
 my $cnt;
@@ -40,7 +40,7 @@ for my $l (split "\n", $out ){
     time_zone  => config()->{logdog}->{timezone},
   );
 
-  # warn $check_date, " ... ", $date;
+  warn $check_date, " ... ", $date;
 
   if ( DateTime->compare( $check_date , $date ) == -1 ){
     set_stdout('line_start');
